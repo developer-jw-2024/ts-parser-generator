2f555ed575745ff4c1e358b798a4c4196cc62045
developer.jacky.wong <developer.jacky.wong@gmail.com>
2024-01-19 16:36:00 +0800

    update export

--- index.ts
+++ index.ts
@@ -1,6 +1,11 @@
-import  { Greeter, Hi }  from './src/GreeterUtils'
+import { NFA, TransferChar, FiniteAutomatonPath, DFAState } from './src/LexicalAnalyzer/NFA'
+import { DFA } from './src/LexicalAnalyzer/DFA'
+import { RegularExpression } from './src/LexicalAnalyzer/RegularExpression'
+import * as SetUtils from './src/Utils/SetUtils'
 
 export {
-    Greeter,
-    Hi
+    NFA, TransferChar, FiniteAutomatonPath, DFAState,
+    DFA,
+    RegularExpression,
+    SetUtils
 }
