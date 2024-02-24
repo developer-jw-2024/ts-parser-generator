f8f0c18478f0accdb452a1e5a174b4acd29ef72b
flyingsaucer <wongsuun@gmail.com>
2024-02-24 10:23:29 +0800

    use annotation to bind the grammar production and method in class

--- test/test3.ts
+++ test/test3.ts
@@ -1,14 +1,43 @@
-//import * as ts from "typescript";
+function first(gp : string) {
+    // console.log("first(): factory evaluated");
+    return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+        // console.log(target, propertyKey, this)
+        const originalMethod = descriptor.value;
+        const className = target.constructor.name;
+        var clazz = target.constructor
+        clazz.prototype[gp] = originalMethod
+        console.log("first(): called");
+    };
+  }
+   
+  function second() {
+    // console.log("second(): factory evaluated");
+    return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+      console.log("second(): called");
+    };
+  }
+   
+  class ExampleClass {
 
-import { convertTextToFunction } from "../src/Utils/JWUtils"
-
-var functionText = `
-function(a : number, b : number) : number {
-    return a + b
-}
-`
-
-var abc : Function =  convertTextToFunction(functionText)
-console.log(abc(5,3))
+    name : string = "abc"
+    
 
 
+    @first("a->b")
+    @second()
+    method() {
+        console.log('I am method', this)
+    }
+  }
+
+  var a = new ExampleClass()
+  a.method()
+  a['a->b']()
+//   for (var member in ExampleClass) { // For each member of the dictionary
+//     if (typeof ExampleClass[member] == "function") { // Is it a function?
+//       if (ExampleClass.hasOwnProperty(member)) { // Not inherited
+//         console.log(member)
+//       }
+//     }
+//   }
+  
