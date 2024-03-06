54252e89b4825ef941c12a7416936705e001d846
developer.jacky.wong <developer.jacky.wong@gmail.com>
2024-03-06 15:40:50 +0800

    update export variable

--- index.ts
+++ index.ts
@@ -1,20 +1,23 @@
-import * as NFA from './src/LexicalAnalyzer/NFA'
-import * as DFA from './src/LexicalAnalyzer/DFA'
-import * as LexicalAnalysis from './src/LexicalAnalyzer/LexicalAnalysis'
-import * as SpecialSymbols from './src/LexicalAnalyzer/SpecialSymbols'
-import * as RegularExpression  from './src/LexicalAnalyzer/RegularExpression'
-import * as LL1 from './src/SyntaxAnalysis/LL1'
-import * as LR from './src/SyntaxAnalysis/LR'
-import * as SyntaxAnalysis from './src/SyntaxAnalysis/SyntaxAnalysis'
+import * as nfa from './src/LexicalAnalyzer/NFA'
+import * as dfa from './src/LexicalAnalyzer/DFA'
+import * as lexical from './src/LexicalAnalyzer/LexicalAnalysis'
+import * as symbols from './src/LexicalAnalyzer/SpecialSymbols'
+import * as regexp  from './src/LexicalAnalyzer/RegularExpression'
+import * as ll1 from './src/SyntaxAnalysis/LL1'
+import * as lr from './src/SyntaxAnalysis/LR'
+import * as syntax from './src/SyntaxAnalysis/SyntaxAnalysis'
 import * as ArrayListUtils from './src/Utils/ArrayListUtils'
 import * as FileUtil from './src/Utils/FileUtil'
 import * as SetUtils from './src/Utils/SetUtils'
 import * as StateSet from './src/Utils/StateSet'
 import * as Utils from './src/Utils/Utils'
 
-export {
-    NFA, DFA, LexicalAnalysis,
-    SpecialSymbols, RegularExpression, LL1,
-    LR, SyntaxAnalysis, ArrayListUtils,
+var utils = {
+    ArrayListUtils,
     FileUtil, SetUtils, StateSet, Utils
+}
+export {
+    nfa, dfa, lexical,
+    symbols, regexp, ll1,
+    lr, syntax, utils
 }
