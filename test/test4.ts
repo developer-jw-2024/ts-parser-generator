0c3ab9e33897583fde248ba64d33de8aa0465da6
flyingsaucer <wongsuun@gmail.com>
2024-03-22 23:30:36 +0800

    change test name

--- test/test4.ts
+++ test/test4.ts
@@ -1,10 +1,29 @@
-class MyClass {
-    t : number 
-    constructor() {
-        this.t = new Date().getTime()
+
+
+function isPrime(n: number) : boolean {
+    var m = Math.ceil(Math.sqrt(n))
+    var f = true
+    for (var i=2;f && i<=m;i++) {
+        f = (n % i)!=0
     }
+    return f
 }
 
-class ABCClass {
-    static my : MyClass = new MyClass()
+function nextPrime(n:number) : number {
+    if (isPrime(n)) {
+        n++
+    }
+    while (!isPrime(n)) n++
+    return n
 }
+var p = [11]
+//console.log(nextPrime(p.at(-1)))
+var v = [500]
+for (var i=2;i<=11;i++) {
+    var pp = p.at(-1)
+    v.push(v.at(-1)-pp)
+    pp = nextPrime(pp)
+    p.push(pp)
+}
+console.log(p)
+console.log(v)
