f85bae23ea2e6eca53a47b1aaaabc8ef1d774516
developer.jacky.wong <developer.jacky.wong@gmail.com>
2024-03-25 16:11:48 +0800

    add test case

--- test/test.ts
+++ test/test.ts
@@ -1,9 +1,7 @@
-import { RegularExpression } from "../src/LexicalAnalyzer/RegularExpression"
-var regExp : RegularExpression = new RegularExpression('[^\\[\\^]+')
-console.log(regExp.test("[^"))
-        // expect(regExp.test("a")).toBe(true)
-        // expect(regExp.test("^")).toBe(false)
-        // expect(regExp.test("[")).toBe(false)
-        // expect(regExp.test("a^")).toBe(false)
-        // expect(regExp.test("[a")).toBe(false)
-        // expect(regExp.test("[^")).toBe(false)
+import { RegularExpression } from "../src/LexicalAnalyzer/RegularExpression";
+import { FileUtils } from "../src/Utils/FileUtil";
+
+var content = FileUtils.readFromFileSystem(`${__dirname}/test`)
+var lines = content.split('\n')
+var regExp : RegularExpression = new RegularExpression(lines[0])
+console.log(regExp.test(lines[1]))
